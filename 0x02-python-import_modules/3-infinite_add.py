2ade63d02c34f96a79d8c89b29ee343a8a41b169
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-06-30 14:32:25 -0400

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+def add(amount, arg):
+    result = 0
+
+    if amount > 1:
+        for i in range(1, amount):
+            result += int(arg[i])
+
+    return result
+
 if __name__ == "__main__":
     from sys import argv
-    add = 0
-    for s in argv[1:]:
-        add += int(s)
-    print("{:d}".format(add))
+    print("{:d}".format(add(len(argv), argv)))
