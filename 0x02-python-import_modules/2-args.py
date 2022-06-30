87ca3bd7af2034dd6f3e4ea6ad4a1cdefb38c904
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-06-30 14:11:13 -0400

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-import sys
+def printArg(amount, arg):
+    if amount is 2:
+        print("{:d} argument:".format(amount - 1))
+        print("{:d}: {}".format(amount - 1, arg[amount - 1]))
+    elif len(arg) > 2:
+        print("{:d} arguments:".format(amount - 1))
+        for index in range(1, amount):
+            print("{:d}: {}".format(index, arg[index]))
+    else:
+        print("{:d} arguments.".format(amount - 1))
 
-if __name__ == '__main__':
-    av = sys.argv
-    l_av = len(av) - 1
-
-    if l_av > 1:
-        print(l_av, 'arguments:')
-        for i in range(1, l_av + 1):
-            print('{:d}: {}'.format(i, av[i]))
-    elif l_av == 1:
-        print(l_av, 'argument:')
-        for i in range(1, l_av + 1):
-            print('{:d}: {}'.format(i, av[i]))
-    elif l_av == 0:
-        print(l_av, 'arguments.')
+if __name__ == "__main__":
+    from sys import argv
+    printArg(len(argv), argv)
