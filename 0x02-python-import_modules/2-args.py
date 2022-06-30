6db77a36e1c187ae9d2de772821a90448dc9ae2a
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-06-30 13:55:30 -0400

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    length = len(sys.argv)
-    if length == 1:
-        print("0 arguments.")
-    else:
-        if length == 2:  # case for 1 argument
-            print("1 argument:")
-        elif length > 2:  # case for more than 1
-            print("{} arguments:".format(length - 1))
-        for i in range(1, length):  # print out the arguments
-            print("{}: {}".format(i, sys.argv[i]))
+import sys
+
+if __name__ == '__main__':
+    """Prints the argument list passed to the program
+    The program takes all the arguments starting from the second
+    and prints the number of arguments and their value
+    """
+    av = sys.argv
+    l_av = len(av) - 1
+
+    if l_av > 1:
+        print(l_av, 'arguments:')
+        for i in range(1, l_av + 1):
+            print('{:d}: {}'.format(i, av[i]))
+    elif l_av == 1:
+        print(l_av, 'argument:')
+        for i in range(1, l_av + 1):
+            print('{:d}: {}'.format(i, av[i]))
+    elif l_av == 0:
+        print(l_av, 'arguments.')
