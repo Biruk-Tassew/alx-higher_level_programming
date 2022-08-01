fd32f5399b4316c052b3c02adbfabeb4f6248abb
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-08-01 08:00:36 -0400

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,15 +5,15 @@ Contains the class MyInt
 
 
 class MyInt(int):
-    """rebel version of an integer, perfect for opposite day!"""
+    """oposite version of an integer"""
     def __new__(cls, *args, **kwargs):
         """create a new instance of the class"""
         return super(MyInt, cls).__new__(cls, *args, **kwargs)
 
     def __eq__(self, other):
-        """what was != is now =="""
+        """swap != and now =="""
         return int(self) != other
 
     def __ne__(self, other):
-        """what was == is now !="""
+        """swap == and now !="""
         return int(self) == other
