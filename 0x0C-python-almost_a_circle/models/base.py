4b3a709bf182c1a9a432c1e4c95bdfcb19242849
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-08-04 05:05:42 -0400

    Done with the basics of the base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains the "Base" class
+"""
 
+class Base:
+    """The base class"""
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """Init the class"""
+        if id:
+            self.id = id
+        else:
+            self.__nb_objects += 1
+            self.id = self.__nb_objects
