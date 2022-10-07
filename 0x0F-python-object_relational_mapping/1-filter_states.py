fb77c6cb20895cbffe25096dc46d5b2640c5378d
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-10-07 22:02:28 +0300

    some fixes

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""Lists all states starting with N"""
+"""
+python script that lists all states from the database hbtn_0e_0_usa with a name
+starting with N
+"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                           user=argv[1], passwd=argv[2], db=argv[3])
-    cur = conn.cursor()
-    cur.execute("""
-SELECT * FROM states ORDER BY states.id ASC
-""")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        if row[1].startswith("N"):
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
             print(row)
-    cur.close()
-    conn.close()
+    cursor.close()
+    db.close()
