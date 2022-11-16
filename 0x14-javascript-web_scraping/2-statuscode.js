a7ad9e6aaad6771169abe639857b43bfbb832e0a
Biruk Tassew <89268568+Biruk-Tassew@users.noreply.github.com>
2022-11-16 10:31:53 -0500

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-const axios = require('axios');
-axios.get(process.argv[2])
-  .then(resp => {
-    console.log('code:', resp.status);
-  })
-  .catch(resp => {
-    console.log('code:', resp.response.status);
-  });
+
+const request = require('request');
+const URL = process.argv[2];
+
+request(URL, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
